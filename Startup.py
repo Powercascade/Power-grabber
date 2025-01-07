a6b878fca3ca50205496cf5bfa0f3e43d7fdd655
Powercascade <powercascade28@gmail.com>
2025-01-07 08:47:04 -0600

    Startup.py

--- Startup.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-current_script = os.path.abspath(__file__)
-startup_folder = os.path.expandvars(r'%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup')
-try:
-    shutil.copy2(current_script, startup_folder)
-except Exception as e:
-    pass

